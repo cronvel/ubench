198379e8704e26c6e4cb0b9e901b93d4b631ad0d
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-02-17 16:15:34 +0100

    ubench, initial publish

--- bench/my-bench.js
+++ bench/my-bench.js
@@ -1,4 +1,18 @@
 
 
 
+var i = 0 ;
+
+bench( 'toto' , 'i++' , function() {
+	i++ ;
+} ) ;
+
+bench( 'toto' , 'i = i + 1' , function() {
+	i = i + 1 ;
+} ) ;
+
+bench( 'toto' , "i = i + '1'" , function() {
+	i = i + '1' ;
+} ) ;
+
 
